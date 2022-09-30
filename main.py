ce294d7793f247c7642a7983db07f69c0aa0fc28
skyrinu <108900820+skyrinu@users.noreply.github.com>
2022-09-30 16:58:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ user_input = input("Please enter the range of numbers in which you want to "
 # Input is taken as a list. List is converted into a range.
 numbers = range(int(user_input[0]), int(user_input[1]) + 1)
 
-# The Numbers which re divided by 3, are printed as "Fizz". Numbers which are
+# The numbers which re divided by 3, are printed as "Fizz". The numbers which are
 # divided by 5, are printed as "Buzz". The numbers which are divided by
-# both, are printed as "FizzBuzz".At the last, the normal numbers are
+# both, are printed as "FizzBuzz". At last, rest of the numbers are
 # printed as it is.
 for number in numbers:
     if number % 3 == 0 and number % 5 == 0:
