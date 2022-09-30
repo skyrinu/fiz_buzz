1cb56eef2ad63fcfc0268f1327af322440da4e8b
skyrinu <108900820+skyrinu@users.noreply.github.com>
2022-09-30 16:51:51 +0530

    Edit main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# Greetings make program interactive.
+print("Welcome to FizzBuzz Generator!")
+
+# Input from user for a range of numbers in which FizzBuzz are wanted to be
+# generated in.
+user_input = input("Please enter the range of numbers in which you want to "
+                   "generate FizzBuzz between in (seperated by a"
+                   " space):\n").split()
+
+# Input is taken as a list. List is converted into a range.
+numbers = range(int(user_input[0]), int(user_input[1]) + 1)
+
+# The Numbers which re divided by 3, are printed as "Fizz". Numbers which are
+# divided by 5, are printed as "Buzz". The numbers which are divided by
+# both, are printed as "FizzBuzz".At the last, the normal numbers are
+# printed as it is.
+for number in numbers:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
